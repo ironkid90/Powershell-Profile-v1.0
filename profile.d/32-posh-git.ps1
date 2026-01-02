e265045d6b0daa7382a1ffc03ccefb1473d894b1
ironkid90 <42446028+ironkid90@users.noreply.github.com>
2026-01-02 07:03:12 +0200

    Fix profile module load warning string

--- profile.d/32-posh-git.ps1
+++ profile.d/32-posh-git.ps1
@@ -1,6 +1,3 @@
 
 # profile.d/32-posh-git.ps1
-
-if (Get-Module -ListAvailable posh-git -ErrorAction SilentlyContinue) {
-    Import-Module posh-git -ErrorAction SilentlyContinue
-}
+# Deprecated: use 32-git-completion.ps1 with $global:PPP.EnableGitModule.
